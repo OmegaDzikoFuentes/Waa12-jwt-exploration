289284ed609d3311aeecea0c6cc63a919eb51c56
Omega Dziko F. <megaoy@yahoo.com>
2024-10-03 20:17:56 -0400

    added parts

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ require('dotenv').config();
 
 // Import package
 
-// Your code here 
+// Your code here
 
 // Define variables - DO NOT MODIFY
 
 // 1. Sign (create) a JWT containing your email address
 let token; // DO NOT MODIFY! Re-assign the token variable below.
 
-// Your code here 
+// Your code here
+token = jwt.sign({ email: process.env.EMAIL }, process.env.JWT_SECRET);
 
 // See the JWT in the console - DO NOT MODIFY
 console.log('JWT:', token);
@@ -19,7 +20,8 @@ console.log('JWT:', token);
 
 let payload; // DO NOT MODIFY! Re-assign the payload variable below.
 
-// Your code here 
+// Your code here
+payload = jwt.decode(token);
 
 // See the decoded payload in the console - DO NOT MODIFY
 console.log('Payload:', payload);
@@ -28,7 +30,8 @@ console.log('Payload:', payload);
 
 let verifiedPayload; // DO NOT MODIFY! Re-assign the verifiedPayload variable below.
 
-// Your code here 
+// Your code here
+verifiedPayload = jwt.verify(token, process.env.JWT_SECRET);
 
 // See the verified payload in the console - DO NOT MODIFY
 console.log('Verified Payload:', verifiedPayload);
@@ -38,7 +41,7 @@ console.log('Verified Payload:', verifiedPayload);
 //    To "try" to get the payload using jwt.verify
 //    Then "catch" the error and log it to the console.
 
-// Your code here 
+// Your code here
 
 // (Optional) Bonus: Catch Error With Expired Token
 // First, set the token's expiration (above) to 1 second
@@ -46,4 +49,4 @@ console.log('Verified Payload:', verifiedPayload);
 //    To "try" to get the payload using jwt.verify (with proper secret)
 //    Then "catch" the error and log it to the console
 
-// Your code here 
+// Your code here
